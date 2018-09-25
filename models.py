334a6620f0d03e53bfa827d7b22447c931f8cc28
SamDepardieu <sam.depardieu@gmail.com>
2018-09-25 13:48:37 +0100

    :card_file_box: Change models

--- models.py
+++ models.py
@@ -2,6 +2,32 @@ from app import db
 from datetime import datetime
 
 
+user_skill = db.Table(
+    'user_skill',
+    db.Column('user_id',
+              db.Integer,
+              db.ForeignKey('user.id'),
+              primary_key=True),
+    db.Column('skill_id',
+              db.Integer,
+              db.ForeignKey('skill.id'),
+              primary_key=True)
+)
+
+
+user_follow = db.Table(
+    'user_follow',
+    db.Column('user_id',
+              db.Integer,
+              db.ForeignKey('user.id'),
+              primary_key=True),
+    db.Column('following_id',
+              db.Integer,
+              db.ForeignKey('following.id'),
+              primary_key=True)
+)
+
+
 class Match(db.Model):
 
     def __init__(self, user_1, user_2, round):
@@ -34,6 +60,17 @@ class User(db.Model):
     email = db.Column(db.String(128), nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
     joined = db.Column(db.DateTime(), default=datetime.now())
+    locked = db.Column(db.Boolean(), nullable=False, default=False)
+
+    skills = db.relationship('Skill', secondary=user_skill, lazy='subquery',
+                             backref=db.backref('user', lazy=True))
+    following = db.relationship(
+        'Following',
+        secondary=user_follow,
+        lazy='subquery',
+        backref=db.backref('user', lazy=True)
+    )
+
     department_id = db.Column(
         db.Integer,
         db.ForeignKey('department.id'),
@@ -59,3 +96,22 @@ class Round(db.Model):
     __tablename__ = "round"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     date = db.Column(db.DateTime(), nullable=False)
+
+
+class Skill(db.Model):
+    def __init__(self, name):
+        self.name = name
+
+    __tablename__ = 'skill'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
+
+
+class Following(db.Model):
+
+    def __init__(self, name):
+        self.name = name
+
+    __tablename__ = 'following'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(128), nullable=False)
