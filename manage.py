81e8cf10f19467bbc104e8149d27c5ad333e73d3
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-09-26 14:17:00 +0100

    Get random matches (#33)
    * :white_check_mark: Add random profiles tests
    
    * :sparkles: Now you can request three unlinked profile from the api
    
    * :art: Anonymise the email

--- manage.py
+++ manage.py
@@ -36,6 +36,11 @@ def import_from_csv():
 
             user = User.query.filter_by(email=row['Email address']).first()
             if not user:
+                email = ''.join([
+                    row['Email address'][:1],
+                    'doe',
+                    row['Email address'][4:]
+                ])
                 user = User(
                     row[
                       'First name'
@@ -43,9 +48,8 @@ def import_from_csv():
                       'First name'
                     ] else '',
                     f"Doe{i}{row['Surname'][3:]}",
-                    row['Email address'],
+                    email,
                     True if row['Active'] == 'Yes' else False,
-                    datetime.now(),
                     department.id
                 )
                 db.session.add(user)
@@ -55,13 +59,24 @@ def import_from_csv():
     with open('datasets/matches.csv') as csvfile:
         matches_csv = csv.DictReader(csvfile, dialect='excel', delimiter=',')
         for i, row in enumerate(matches_csv):
-            user = User.query.filter_by(email=row['Email']).first()
+            base_email = ''.join([
+                row['Email'][:1],
+                'doe',
+                row['Email'][4:]
+            ])
+            user = User.query.filter_by(email=base_email).first()
             if not user:
                 continue
             for i in range(1, 27):
                 if row.get(f'R{i}'):
+                    email = row.get(f'R{i}')
+                    email = ''.join([
+                        email[:1],
+                        'doe',
+                        email[4:]
+                    ])
                     user2 = User.query.filter_by(
-                        email=row.get(f'R{i}')
+                        email=email
                     ).first()
                     if user2:
                         match = Match(user.id, user2.id, round.id)
