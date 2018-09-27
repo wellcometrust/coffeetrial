b6879083087e02106ec5baea25a224f57d50cd09
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-09-27 14:58:01 +0100

    Add a view to manually add a user (#37)
    * :sparkles: Testing in manage.py, no more excuses to not test your code
    
    * :white_check_mark: Add tests for the matches list admin view
    
    * :sparkles: Add the admin matches list view
    
    * :sparkles: Add admin user add and tests

--- tests/test_admin_user.py
+++ tests/test_admin_user.py
@@ -1,6 +1,15 @@
 import json
 import unittest
+from app import db
 from tests.base import BaseTestCase
+from models import User, Department
+
+
+def _add_department():
+    department = Department('test')
+    db.session.add(department)
+    db.session.commit()
+    return department
 
 
 class TestUserService(BaseTestCase):
@@ -9,10 +18,29 @@ class TestUserService(BaseTestCase):
     def test_user_ping(self):
         """Ensure the /ping route behaves correctly."""
         with self.client:
-            response = self.client.get('/admin/user/ping')
+            response = self.client.get('/admin/users/ping')
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 200)
             self.assertIn('pong', data['message'])
+            self.assertIn('success', data['status'])
+
+    def test_user_add(self):
+        department = _add_department()
+        with self.client:
+            response = self.client.post(
+                '/admin/users',
+                data=json.dumps({
+                    'firstname': 'john',
+                    'lastname': 'doe',
+                    'email': 'john.doe@example.com',
+                    'active': True,
+                    'department_id': department.id,
+                }),
+                content_type='application/json',
+            )
+            data = json.loads(response.data.decode(encoding='utf-8'))
+            self.assertEqual(response.status_code, 201)
+            self.assertIn('john.doe@example.com was added!', data['message'])
             self.assertIn('success', data['status'])
 
 
