5a73b6d89c6a79879d38d2720f125709284bc021
eleanorreem <you@example.com>
2018-09-28 11:33:39 +0100

    add styling

--- frontend/src/components/colleague.js
+++ frontend/src/components/colleague.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 import Button from './button';
-export default ({firstname, lastname, email, following, skills, preferences, blurred, link }) =>
+export default ({firstname, lastname, email, following, skills, preferences, blurred, link, buttonText }) =>
     <div className='card'>
         <h2 className={blurred ? 'blurred-text' : ''}>{firstname + ' ' + lastname }</h2>
         <p><span className='label'>Following:</span> {following}</p>
         <p><span className='label'>Skills:</span> {skills}</p>
         <p><span className='label'>Preferences:</span> {preferences}</p>
-        <Button text='select' link={link ? link.href : '/'}/>
+        <Button text={buttonText} link={link ? link : {href: '/', state: {}}}/>
     </div>
--- frontend/src/views/confirmation.js
+++ frontend/src/views/confirmation.js
@@ -1,17 +1,38 @@
-import Button from '../components/button';
-import Colleague from '../components/colleague';
-import Header from '../components/header';
 import React from 'react';
+import Colleague from '../components/colleague';
+export default class Confirmation extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: '1',
+            match: props.location.state.match,
+        };
+    }
 
+    render() {
+        const {match} = this.state;
+        return (
+            <div className="page">
+                <h1>Looking forward to meet {match.firstname} {match.lastname}?</h1>
+                <p>We have sent an email to both of you so that you can arrange your Coffee trial.
+                    Please make sure you manage to meet up by 30. November 2018 at the latest.
+                    You will meet:
+                </p>
+                <Colleague
+                    firstname={match.firstname}
+                    lastname={match.lastname}
+                    preferences='Instant coffee'
+                    skills='SQL, Databases'
+                    following='Trustnet fanclub'
+                    buttonText={'Invite ' + match.firstname + ' ' + match.lastname}
+                    link={{
+                        href: '/',
+                        state: {match: match}
+                    }}
+                />
+            </div>
+        );
 
-export default () =>
-    <div className="page">
-        <img src ='coffee-gif' src="https://www.youreduaction.it/wp-content/uploads/2015/06/ci-offri-un-caff%C3%A8.gif"/>
-        <h1>Looking forward to meet Christine?</h1>
-        <p>We have sent an email to both of you so that you can arrange your Coffee trial.
-            Please make sure you manage to meet up by 30. November 2018 at the latest.
-            You will meet:
-        </p>
-        <Colleague name='fred' preferences='cappicuno' skills='SQL, Databases' following='Trustnet fanclub'/>
-        <Button text={'Invite Christine'} link='/'/>
-    </div>
+    }
+
+}
