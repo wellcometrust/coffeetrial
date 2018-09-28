1b1750dbfa4d8f6301f2573d9a774a54e1294d4b
Samuel Depardieu <6536364+SamDepardieu@users.noreply.github.com>
2018-09-28 11:59:52 +0100

    Add a match random route (#44)
    * :art: Refactor matching routes
    
    * :sparkles: Add match a random user
    
    * :white_check_mark: Add tests for the new route

--- client/matching.py
+++ client/matching.py
@@ -11,6 +11,40 @@ matching_blueprint = Blueprint(
 )
 
 
+def get_random_user_set(user):
+    random.seed()
+    matches = Match.query.filter_by(
+        user_1=user.id
+    ).all()
+    matches = [match.user_2 for match in matches]
+    matches.append(user.id)
+    users = User.query.filter(
+        User.id.notin_(matches)
+    ).filter_by(locked=False).filter_by(active=True)
+    return [user.to_dict() for user in users.all()]
+
+
+def match_users(user_1, user_2):
+    if user_1.locked or user_2.locked:
+        return {'status': False, 'msg': 'user locked'}
+
+    if not (user_1.active and user_2.active):
+        return {'status': False, 'msg': 'user inactive'}
+
+    current_round = Round.query.order_by(Round.date.desc()).first()
+
+    match = Match(user_1.id, user_2.id, current_round.id)
+    db.session.add(match)
+
+    user_1.locked = True
+    user_2.locked = True
+
+    db.session.add(user_1)
+    db.session.add(user_2)
+    db.session.commit()
+    return {'status': True, 'object': match.to_dict()}
+
+
 @matching_blueprint.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify(
@@ -28,16 +62,8 @@ def index():
 
 @matching_blueprint.route('/match/<int:user>', methods=['GET'])
 def get_random_profiles(user):
-    random.seed()
-    matches = Match.query.filter_by(
-        user_1=user
-    ).all()
-    matches = [match.user_2 for match in matches]
-    matches.append(user)
-    users = User.query.filter(
-        User.id.notin_(matches)
-    ).filter_by(locked=False).filter_by(active=True)
-    users = [user.to_dict() for user in users.all()]
+    user = User.query.get_or_404(user)
+    users = get_random_user_set(user)
     if len(users) > 2:
         users = random.sample(users, 3)
     return jsonify(users), 200
@@ -53,25 +79,40 @@ def set_match():
     user_1 = User.query.get_or_404(post_data.get('user_1_id'))
     user_2 = User.query.get_or_404(post_data.get('user_2_id'))
 
-    if user_1.locked or user_2.locked:
-        response_item['message'] = 'Couldn\'t match users (user locked).'
+    match = match_users(user_1, user_2)
+    if not match['status']:
+        response_item['message'] = f"Couldn't match users ({match['msg']})."
         return jsonify(response_item), 400
 
-    if not (user_1.active and user_2.active):
-        response_item['message'] = 'Couldn\'t match users (user inactive).'
+    response_item['match'] = match['object']
+    response_item['message'] = 'Users successfully matched!'
+    response_item['status'] = 'success'
+    return jsonify(response_item), 201
+
+
+@matching_blueprint.route('/match/random', methods=['POST'])
+def set_match_random():
+    response_item = {
+        'status': 'failed',
+        'message': '',
+    }
+    post_data = request.get_json()
+    user = User.query.get_or_404(post_data.get('user_id'))
+
+    users = get_random_user_set(user)
+    if not users:
+        response_item['message'] = 'No free user to match with.'
         return jsonify(response_item), 400
 
-    current_round = Round.query.order_by(Round.date.desc()).first()
+    user_2 = random.choice(users)
+    user_2 = User.query.get_or_404(user_2['id'])
 
-    match = Match(user_1.id, user_2.id, current_round.id)
-    db.session.add(match)
+    match = match_users(user, user_2)
+    if not match['status']:
+        response_item['message'] = f"Couldn't match users ({match['msg']})."
+        return jsonify(response_item), 400
 
-    user_1.locked = True
-    user_2.locked = True
-
-    db.session.add(user_1)
-    db.session.add(user_2)
-    db.session.commit()
+    response_item['match'] = match['object']
     response_item['message'] = 'Users successfully matched!'
     response_item['status'] = 'success'
     return jsonify(response_item), 201
