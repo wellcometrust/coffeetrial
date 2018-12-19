860328fc4872f23a9102d9ee8e010e09f46ee8bf
SamDepardieu <sam.depardieu@gmail.com>
2018-12-19 17:12:03 +0000

    Add a (draft) login screen

---
+++ frontend/src/views/login.js
@@ -0,0 +1,7 @@
+import React from 'react';
+export default () =>
+            <div className="page">
+                <h1> Welcome </h1>
+                <p> Random Coffeetrials are a fun way to get to know new colleagues. </p>
+                <p> Please login to use this app.</p>
+            </div>
