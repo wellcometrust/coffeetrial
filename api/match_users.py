f534f2e24746de9b3803f3127a885df6b7f9e679
SamDepardieu <sam.depardieu@gmail.com>
2019-03-25 18:07:30 +0000

    Add csv import

--- api/match_users.py
+++ api/match_users.py
@@ -1,4 +1,8 @@
-from models import Match, Round, User
+import csv
+
+from datetime import datetime
+
+from models import Match, Round, User, Department
 from app import db
 
 
@@ -15,10 +19,9 @@ def get_random_user_set(user):
     matches = [match.user_2 for match in matches]
     matches.append(user.id)
     users = User.query.filter(
-        User.id.notin_(matches)
-    ).filter(
+        User.id.notin_(matches),
         User.department_id.notin_([user.department_id])
-    ).filter_by(locked=False).filter_by(active=True)
+    ).filter_by(locked=False, active=True, is_admin=False)
     return users.all()
 
 
@@ -57,7 +60,10 @@ def match_users(user_1, user_2):
 def unlock_all_users():
     """Set all user status to "unlocked" to match them this round."""
 
-    users = User.query.all()
+    users = User.query.filter_by(
+        active=True,
+        is_admin=False
+    ).all()
     for user in users:
         user.locked = False
         db.session.add(user)
@@ -103,3 +109,74 @@ def match_all_users():
     ).all()
 
     return [u.to_dict() for u in unmatched_users]
+
+
+def import_from_csv(csv_path, matches_csv_path):
+    departments = set([])
+    users = []
+    matches = []
+    with open(csv_path) as csvfile:
+        users_csv = csv.DictReader(csvfile, dialect='excel', delimiter=',')
+        for i, row in enumerate(users_csv):
+            departments.add(row['Department'])
+            department = Department.query.filter_by(
+              name=row['Department']
+            ).first()
+            if not department:
+                department = Department(row['Department'])
+                db.session.add(department)
+
+            user = User.query.filter_by(email=row['Email address']).first()
+            if not user:
+                email = ''.join([
+                    row['Email address'][:1],
+                    'doe',
+                    row['Email address'][4:]
+                ])
+                user = User(
+                  firstname=row['First name'][0] if row['First name'] else '',
+                  lastname=f"Doe{i}{row['Surname'][3:]}",
+                  email=email,
+                  active=True if row['Active'] == 'Yes' else False,
+                  department_id=department.id
+                )
+                db.session.add(user)
+            users.append(row)
+    db.session.commit()
+
+    with open(matches_csv_path) as csvfile:
+        matches_csv = csv.DictReader(csvfile, dialect='excel', delimiter=',')
+        for i, row in enumerate(matches_csv):
+            base_email = ''.join([
+                row['Email'][:1],
+                'doe',
+                row['Email'][4:]
+            ])
+            user = User.query.filter_by(email=base_email).first()
+            if not user:
+                continue
+            for i in range(1, 27):
+                if row.get(f'R{i}'):
+                    round = Round.query.filter_by(id=i).first()
+                    if not round:
+                        round = Round(datetime.now())
+                        db.session.add(round)
+                        db.session.commit()
+                    email = row.get(f'R{i}')
+                    email = ''.join([
+                        email[:1],
+                        'doe',
+                        email[4:]
+                    ])
+                    user2 = User.query.filter_by(
+                        email=email
+                    ).first()
+                    if user2:
+                        match = Match(user.id, user2.id, round.id)
+                        db.session.add(match)
+                        matches.append(match)
+    print(
+        f'[+] Imported {len(matches)} matches and {len(users)}',
+        f'users from {len(departments)} departments.'
+    )
+    db.session.commit()
