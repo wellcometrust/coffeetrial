982617f19dbaab43d4f0c743afe3180b4c8814ff
SamDepardieu <sam.depardieu@gmail.com>
2019-03-25 18:08:39 +0000

    Add new su command and move csv import in match_users file

--- api/manage.py
+++ api/manage.py
@@ -1,9 +1,11 @@
-import csv
 import unittest
+import getpass
+import click
 from datetime import datetime
 from flask.cli import FlaskGroup
-from models import User, Department, Round, Match
-from match_users import unlock_all_users, match_all_users
+from models import User, Round
+from match_users import unlock_all_users, match_all_users, import_from_csv
+from admin.user import _get_hashed_password
 
 from app import create_app, db
 
@@ -19,75 +21,8 @@ def recreate_db():
 
 
 @cli.command()
-def import_from_csv():
-    departments = set([])
-    users = []
-    matches = []
-    with open('datasets/dataset.csv') as csvfile:
-        users_csv = csv.DictReader(csvfile, dialect='excel', delimiter=',')
-        for i, row in enumerate(users_csv):
-            departments.add(row['Department'])
-            department = Department.query.filter_by(
-              name=row['Department']
-            ).first()
-            if not department:
-                department = Department(row['Department'])
-                db.session.add(department)
-
-            user = User.query.filter_by(email=row['Email address']).first()
-            if not user:
-                email = ''.join([
-                    row['Email address'][:1],
-                    'doe',
-                    row['Email address'][4:]
-                ])
-                user = User(
-                  firstname=row['First name'][0] if row['First name'] else '',
-                  lastname=f"Doe{i}{row['Surname'][3:]}",
-                  email=email,
-                  active=True if row['Active'] == 'Yes' else False,
-                  department_id=department.id
-                )
-                db.session.add(user)
-            users.append(row)
-    db.session.commit()
-
-    with open('datasets/matches.csv') as csvfile:
-        matches_csv = csv.DictReader(csvfile, dialect='excel', delimiter=',')
-        for i, row in enumerate(matches_csv):
-            base_email = ''.join([
-                row['Email'][:1],
-                'doe',
-                row['Email'][4:]
-            ])
-            user = User.query.filter_by(email=base_email).first()
-            if not user:
-                continue
-            for i in range(1, 27):
-                if row.get(f'R{i}'):
-                    round = Round.query.filter_by(id=i).first()
-                    if not round:
-                        round = Round(datetime.now())
-                        db.session.add(round)
-                        db.session.commit()
-                    email = row.get(f'R{i}')
-                    email = ''.join([
-                        email[:1],
-                        'doe',
-                        email[4:]
-                    ])
-                    user2 = User.query.filter_by(
-                        email=email
-                    ).first()
-                    if user2:
-                        match = Match(user.id, user2.id, round.id)
-                        db.session.add(match)
-                        matches.append(match)
-    print(
-        f'[+] Imported {len(matches)} matches and {len(users)}',
-        f'users from {len(departments)} departments.'
-    )
-    db.session.commit()
+def import_csv():
+    import_from_csv('datasets/dataset.csv', 'datasets/matches.csv')
 
 
 @cli.command()
@@ -117,6 +52,27 @@ def new_round():
     print(unmatched)
 
     return unmatched
+
+
+@cli.command()
+@click.argument('firstname')
+@click.argument('lastname')
+@click.argument('email')
+@click.option('--password', prompt=True, hide_input=True,
+              confirmation_prompt=True)
+def add_superuser(firstname, lastname, email, password=None):
+    if not password:
+        password = getpass()
+    s_user = User(
+        firstname=firstname,
+        lastname=lastname,
+        email=email,
+        active=True,
+    )
+    s_user.password = _get_hashed_password(password)
+    s_user.is_admin = True
+    db.session.add(s_user)
+    db.session.commit()
 
 
 if __name__ == '__main__':
