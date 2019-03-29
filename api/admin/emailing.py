7589af64ec48eda40cd87d938d836a7b8303d87e
SamDepardieu <sam.depardieu@gmail.com>
2019-03-29 14:39:48 +0000

    Add unmatched users email

--- api/admin/emailing.py
+++ api/admin/emailing.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify
 from client.authentication import is_admin
-from mails import test_match_email
 
 
 emailing_blueprint = Blueprint(
@@ -13,18 +12,6 @@ emailing_blueprint = Blueprint(
 @emailing_blueprint.route('/admin/emailing/ping', methods=['GET'])
 @is_admin
 def ping_pong():
-    return jsonify(
-        {
-            'status': 'success',
-            'message': 'pong'
-        }
-    )
-
-
-@emailing_blueprint.route('/admin/emailing/test', methods=['GET'])
-@is_admin
-def test_emails():
-    test_match_email()
     return jsonify(
         {
             'status': 'success',
