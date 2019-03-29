0e3b8ec6c516a98d4432cac83f9389a04ee8458c
SamDepardieu <sam.depardieu@gmail.com>
2019-03-29 11:33:41 +0000

    Add new view to app

--- api/app.py
+++ api/app.py
@@ -18,15 +18,17 @@ def create_app(script_info=None):
     app.config.from_object(app_settings)
 
     db.init_app(app)
-    from admin.user import user_blueprint
+    from admin.user_management import user_management_blueprint
     from admin.round import round_blueprint
     from admin.matches import matches_blueprint
-    app.register_blueprint(user_blueprint)
+    app.register_blueprint(user_management_blueprint)
     app.register_blueprint(round_blueprint)
     app.register_blueprint(matches_blueprint)
 
     from client.matching import matching_blueprint
+    from client.user import user_blueprint
     from client.authentication import auth_blueprint
+    app.register_blueprint(user_blueprint)
     app.register_blueprint(matching_blueprint)
     app.register_blueprint(auth_blueprint)
 
