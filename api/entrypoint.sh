ae0a07e4725f27773eae585ef2e666c1ba0a23e6
SamDepardieu <sam.depardieu@gmail.com>
2019-06-25 16:31:16 +0100

    entrypoint now also build DB

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [ "$FLASK_ENV" == "development" ] ; then
-  python3 manage.py run -h0.0.0.0;
+if [ "$FLASK_ENV" = "development" ]; then
+    python3 manage.py run -h0.0.0.0;
 else
-  gunicorn -b 0.0.0.0:5000 manage:app
+    gunicorn -b 0.0.0.0:5000 manage:app
 fi
 
 exit 0
